12e09a84ca37e43f453df4cbd822f4c5eddea680
Johan Nordström <johan@digitalvillage.se>
2021-04-12 13:23:14 +0200

    Added navbar to starter template.

--- index.php
+++ index.php
@@ -12,6 +12,29 @@
 </head>
 
 <body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#">Site Title</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link 1</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link 2</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
 	<h1>Hello, world!</h1>
 
 	<!-- Bootstrap Bundle with Popper -->
