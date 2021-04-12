465a2950496b09ee1b5c40edf4b38bd5ae482886
Johan Nordström <johan@digitalvillage.se>
2021-04-12 13:49:45 +0200

    A very basic loop.

--- index.php
+++ index.php
@@ -2,7 +2,25 @@
 get_header();
 ?>
 
-	<h1>Hello, world!</h1>
+<h1>index</h1>
+
+<!-- Do we have any posts to display? -->
+<?php if (have_posts()) : ?>
+	<!-- Yay, we has posts do display! -->
+	<?php while (have_posts()) : ?>
+		<!-- Start post -->
+		<?php
+			// Load next post to display
+			the_post();
+		?>
+		<h2><?php the_title(); ?></h2>
+
+		<?php the_content(); ?>
+		<!-- End post -->
+	<?php endwhile; ?>
+<?php else: ?>
+	<p>Sorry, no posts found.</p>
+<?php endif; ?>
 
 <?php
 get_footer();
