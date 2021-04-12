560cb3172ebb7bc47148e15944cce9e15da11c80
Johan Nordström <johan@digitalvillage.se>
2021-04-12 14:13:41 +0200

    Added article-wrapper with card around each post.

--- index.php
+++ index.php
@@ -2,25 +2,37 @@
 get_header();
 ?>
 
-<h1>index</h1>
+<div class="container">
 
-<!-- Do we have any posts to display? -->
-<?php if (have_posts()) : ?>
-	<!-- Yay, we has posts do display! -->
-	<?php while (have_posts()) : ?>
-		<!-- Start post -->
-		<?php
-			// Load next post to display
-			the_post();
-		?>
-		<h2><?php the_title(); ?></h2>
+	<h1>index</h1>
 
-		<?php the_content(); ?>
-		<!-- End post -->
-	<?php endwhile; ?>
-<?php else: ?>
-	<p>Sorry, no posts found.</p>
-<?php endif; ?>
+	<hr />
+
+	<!-- Do we have any posts to display? -->
+	<?php if (have_posts()) : ?>
+		<!-- Yay, we has posts do display! -->
+		<?php while (have_posts()) : ?>
+			<!-- Start post -->
+			<?php
+				// Load next post to display
+				the_post();
+			?>
+			<article class="card mb-4">
+				<div class="card-body">
+					<h2 class="card-title h4"><?php the_title(); ?></h2>
+
+					<div class="card-text">
+						<?php the_content(); ?>
+					</div>
+				</div>
+			</article>
+			<!-- End post -->
+		<?php endwhile; ?>
+	<?php else: ?>
+		<p>Sorry, no posts found.</p>
+	<?php endif; ?>
+
+</div>
 
 <?php
 get_footer();
