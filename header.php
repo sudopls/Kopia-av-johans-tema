f5d3bfe889b758b37b1329261319d1c8797d00c3
Johan Nordström <johan@digitalvillage.se>
2021-04-12 15:03:01 +0200

    Output site title and wp_head/wp_footer.

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
@@ -8,13 +8,18 @@
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-	<title>Hello, world!</title>
+	<title><?php bloginfo('name'); ?></title>
+
+	<?php wp_head(); ?>
 </head>
 
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container">
-			<a class="navbar-brand" href="/">Site Title</a>
+			<a class="navbar-brand" href="/">
+				<?php bloginfo('name'); ?>
+			</a>
+
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
