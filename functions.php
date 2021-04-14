175068feeaea49236160eb535d4e54c3beaa2a2e
Johan Nordström <johan@digitalvillage.se>
2021-04-14 11:52:58 +0200

    Blog sidebar.

--- functions.php
+++ functions.php
@@ -37,3 +37,21 @@ add_filter('the_excerpt', 'mbt_filter_the_excerpt');
  * Declare support for 'title-tag'
  */
 add_theme_support('title-tag');
+
+/**
+ * Register widget areas (a.k.a. sidebars).
+ *
+ * @return void
+ */
+function mbt_widgets_init() {
+	register_sidebar([
+		'name' => 'Blog Sidebar',
+		'id' => 'blog-sidebar',
+		'description' => 'Sidebar on blog index, category archive and single blog posts.',
+		'before_widget' => '<div id="%1$s" class="card mb-3 widget %2$s"><div class="card-body">',
+		'after_widget' => '</div></div>',
+		'before_title' => '<h3 class="widget-title h5">',
+		'after_title' => '</h3>',
+	]);
+}
+add_action('widgets_init', 'mbt_widgets_init');
--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,4 @@
 <h2 class="h4">Sidebar</h2>
 
-<div class="card mb-4">
-	<div class="card-body">
-		<div class="card-title h5">Widget 1</div>
-		<p class="mb-0">Lorem Ipsum Widgetus Maximus</p>
-	</div>
-</div>
-
-<div class="card mb-4">
-	<div class="card-body">
-		<div class="card-title h5">Widget 2</div>
-		<p class="mb-0">Lorem Ipsum Widgetus Maximus</p>
-	</div>
-</div>
-
-<div class="card mb-4">
-	<div class="card-body">
-		<div class="card-title h5">Widget 3</div>
-		<p class="mb-0">Lorem Ipsum Widgetus Maximus</p>
-	</div>
-</div>
+<?php
+dynamic_sidebar('blog-sidebar');
