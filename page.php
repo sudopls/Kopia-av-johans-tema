68c7cd3905bc908703d3dba0eaf3098632300bd6
Johan Nordström <johan@digitalvillage.se>
2021-04-14 09:48:52 +0200

    Page template.

---
+++ page.php
@@ -0,0 +1,36 @@
+<?php
+get_header();
+?>
+
+<div class="container">
+
+	<pre>page.php</pre>
+
+	<hr />
+
+	<!-- Do we have any posts to display? -->
+	<?php if (have_posts()) : ?>
+		<!-- Yay, we has posts do display! -->
+		<?php while (have_posts()) : ?>
+			<!-- Start post -->
+			<?php
+				// Load next post to display
+				the_post();
+			?>
+			<article>
+				<h1 class="text-center my-5"><?php the_title(); ?></h1>
+
+				<div class="card-text">
+					<?php the_content(); ?>
+				</div>
+			</article>
+			<!-- End post -->
+		<?php endwhile; ?>
+	<?php else: ?>
+		<p>Sorry, no post found.</p>
+	<?php endif; ?>
+
+</div>
+
+<?php
+get_footer();
