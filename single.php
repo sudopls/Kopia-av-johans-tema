f2b264c3a7decf664afa8b1512ffbb70c985779d
Johan Nordström <johan@digitalvillage.se>
2021-04-14 10:01:56 +0200

    A nice sidebar

--- single.php
+++ single.php
@@ -8,33 +8,41 @@ get_header();
 
 	<hr />
 
-	<!-- Do we have any posts to display? -->
-	<?php if (have_posts()) : ?>
-		<!-- Yay, we has posts do display! -->
-		<?php while (have_posts()) : ?>
-			<!-- Start post -->
-			<?php
-				// Load next post to display
-				the_post();
-			?>
-			<article>
-				<h1><?php the_title(); ?></h1>
+	<div class="row">
+		<div class="col-md-9">
+			<!-- Do we have any posts to display? -->
+			<?php if (have_posts()) : ?>
+				<!-- Yay, we has posts do display! -->
+				<?php while (have_posts()) : ?>
+					<!-- Start post -->
+					<?php
+						// Load next post to display
+						the_post();
+					?>
+					<article>
+						<h1><?php the_title(); ?></h1>
 
-				<div class="card-meta text-muted small mb-2">
-					Post published <?php echo get_the_date(); ?> at <?php the_time(); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
-				</div>
+						<div class="card-meta text-muted small mb-2">
+							Post published <?php echo get_the_date(); ?> at <?php the_time(); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
+						</div>
 
-				<div class="card-text">
-					<?php the_content(); ?>
-				</div>
-			</article>
-			<!-- End post -->
-		<?php endwhile; ?>
-	<?php else: ?>
-		<p>Sorry, no post found.</p>
-	<?php endif; ?>
+						<div class="card-text">
+							<?php the_content(); ?>
+						</div>
+					</article>
+					<!-- End post -->
+				<?php endwhile; ?>
+			<?php else: ?>
+				<p>Sorry, no post found.</p>
+			<?php endif; ?>
+		</div><!-- /.col-md-9 -->
 
-</div>
+		<div class="col-md-3">
+			<?php include('sidebar.php'); ?>
+		</div><!-- /.col-md-3 -->
+
+	</div><!-- /.row -->
+</div><!-- /.container -->
 
 <?php
 get_footer();
