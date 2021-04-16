8399d038b4221e68741fe9a4d1a3a1e850b08694
Johan Nordström <johan@digitalvillage.se>
2021-04-16 10:23:58 +0200

    Enqueued Bootstrap JavaScript.

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@ require_once('includes/Bootstrap_5_WP_Nav_Menu_Walker.php');
 add_theme_support('title-tag');
 
 function mbt_register_scripts_and_styles() {
+	/**
+	 * Styles
+	 */
 	// Bootstrap 5
 	wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css', [], '5.0.0-beta3', 'all');
 
@@ -16,6 +19,11 @@ function mbt_register_scripts_and_styles() {
 
 	// Print styles
 	wp_enqueue_style('mbt-print', get_parent_theme_file_uri('print.css'), ['bootstrap'], '0.1', 'print');
+
+	/**
+	 * Scripts
+	 */
+	wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js', [], '5.0.0-beta3', true);
 }
 add_action('wp_enqueue_scripts', 'mbt_register_scripts_and_styles');
 
