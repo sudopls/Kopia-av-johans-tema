3d36b562bbd57c7fe0e55b2cfe8cd0e5d09506ce
Johan Nordström <johan@digitalvillage.se>
2021-04-19 10:59:21 +0200

    Show featured image on single blog posts.

--- single.php
+++ single.php
@@ -20,15 +20,20 @@ get_header();
 						the_post();
 					?>
 					<article>
-						<h1><?php the_title(); ?></h1>
-
-						<div class="card-meta text-muted small mb-2">
-							Post published <?php echo get_the_date(); ?> at <?php the_time(); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
-						</div>
+						<header class="mb-3">
+							<?php the_post_thumbnail('large', ['class' => 'img-fluid mb-2']); ?>
+							<h1><?php the_title(); ?></h1>
+						</header>
 
 						<div class="card-text">
 							<?php the_content(); ?>
 						</div>
+
+						<footer>
+							<div class="card-meta text-muted small mb-2">
+								Post published <?php echo get_the_date(); ?> at <?php the_time(); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>
+							</div>
+						</footer>
 					</article>
 					<!-- End post -->
 				<?php endwhile; ?>
